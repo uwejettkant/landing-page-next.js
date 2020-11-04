608598d5060861faf0299f4954335550d32c84d8
Uwe Jettkant <uj@macbook-pro-6.betahaushh.lan>
2020-11-04 11:15:36 +0100

    added content component styling
    Signed-off-by: Uwe Jettkant <uj@macbook-pro-6.betahaushh.lan>

--- components/Content.js
+++ components/Content.js
@@ -5,17 +5,17 @@ export default function Content () {
   return (
     <ContentWrapper class='content-part'>
       <h2>Get a step by step guide</h2>
-      <p class='subline'>For Free</p>
+      <Subline class='subline'>For Free</Subline>
       <p class='main-content'>You want to know how to import physical products worldwide? Then this course is for you!<br />
         Especially when you are still at the beginning of an e-commerce business, there are often numerous uncertainties.  You will make mistakes that cost not only time but also money.
         With the help of this online course, you can avoid additional costs and a delay in delivery. In a simple step-by-step guide, this course will guide you through each phase of the import process and give you useful tips, recommendations and a checklist to make your first import a success.
       </p>
-      <form class='contact-details'>
+      <Form class='contact-details'>
         <input type='text' required placeholder='name' />
         <input type='text' required placeholder='e-mail' />
         <button>register now</button>
         <p class='disclaimer'>We'll never share your information without your permission</p>
-      </form>
+      </Form>
     </ContentWrapper>
   )
 }
@@ -36,4 +36,68 @@ const ContentWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     }
+
+    h2 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin: 0;
+
+  @media(min-width: 650px) {
+    font-size: 1.75rem;
+    align-self: center;
+  }
+
+    }
+
+    h2 + p {
+  font-family: 'roboto', 'san-serif';
+  color: #022ef0;
+  font-weight: 900;
+  text-transform: uppercase; 
+}
+`
+
+const Subline = styled.p`
+  font-size: 1.125rem;
+`
+
+const Form = styled.form`
+    width: 80%;
+  max-width: 300px;
+  margin-top: 1.5em;
+
+  input, button {
+  width: 100%;
+  border-radius: 25px;
+}
+
+input {
+  font-family: 'roboto';
+  text-align: inherit;
+  margin-bottom: 1.5em;
+  padding: .5em;
+  border: 1px solid #404040;
+  outline: none;
+}
+
+button {
+  font-size: 1.125rem;
+  padding: .5em 1em;
+  font-weight: 900;
+  letter-spacing: 0.2px;
+  border: 0;
+  display: inline-block;
+  background: #93e8f5;
+  text-transform: uppercase;
+  color: #fff;
+  cursor: pointer;
+  background-image: linear-gradient(45deg, red, blue);
+  transform: scale(1);
+  transition: transform 250ms;
+  margin-bottom: .5em;
+}
+
+button:hover, button:focus {
+  transform: scale(1.1);
+}
 `
